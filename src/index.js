5c814401644661e739399bd2816ef2ae653d5790
Kinga Gogowska <glogowska.kinga95@gmail.com>
2020-07-22 15:15:02 +0200

    feat: skills desktop styles + hero decoration

--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ import "../src/icon/github-blue.svg";
 import "../src/icon/instagram.svg";
 import "../src/icon/facebook.svg";
 import "../src/icon/speech.svg";
+import "../src/icon/hero-left.svg";
+import "../src/icon/hero-right.svg";
 console.log("hello wossrld!");
