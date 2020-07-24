c3f3df348301e429bbf2cb2e1a5645eedb0d711c
Kinga Gogowska <glogowska.kinga95@gmail.com>
2020-07-24 10:03:06 +0200

    feat: links in new tab, hand animation

--- src/index.js
+++ src/index.js
@@ -6,4 +6,6 @@ import "../src/icon/facebook.svg";
 import "../src/icon/speech.svg";
 import "../src/icon/hero-left.svg";
 import "../src/icon/hero-right.svg";
-console.log("hello wossrld!");
+
+
+console.log("Hi I'm Kinga nice to meet you");
